ba0ac9135df5096b42d295ebf78f6ae07b850b19
muhammad-suleman-bfx <muhammadsuleman.tahir@albaik.com>
2026-08-17 16:43:44 +0300

    added menu_item schema, models, migrations, associations, validations

---
+++ app/models/menu_item.rb
@@ -0,0 +1,7 @@
+class MenuItem < ApplicationRecord
+  has_many :branches, through: :branch_menu_items
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :description, length: { maximum: 2000 }
+  validates :base_availability, inclusion: { in: [ true, false ] }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_17_000001) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_17_000002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_17_000001) do
     t.text "address", null: false
     t.decimal "latitude", precision: 10, scale: 8, null: false
     t.decimal "longitude", precision: 11, scale: 8, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "menu_items", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.boolean "base_availability", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
