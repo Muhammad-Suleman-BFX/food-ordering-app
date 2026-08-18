81d118fd9f9c5b14c6b2ce86c1d9d3d43e9b9c63
muhammad-suleman-bfx <muhammadsuleman.tahir@albaik.com>
2026-08-18 16:30:16 +0300

    chore: added scope in branch_menu_item model

--- app/models/branch_menu_item.rb
+++ app/models/branch_menu_item.rb
@@ -7,6 +7,10 @@ class BranchMenuItem < ApplicationRecord
   validates :menu_item_id, uniqueness: { scope: :branch_id, message: "is already associated with this branch" }
   validate :menu_item_must_be_available_if_branch_item_available
 
+  scope :available, -> { where(menu_item_availability: true).joins(:menu_item).where(menu_items: { base_availability: true }) }
+  scope :by_branch, ->(branch_id) { where(branch_id: branch_id) }
+  scope :ordered_by_price, -> { order(menu_item_price: :asc) }
+
   private
 
   def menu_item_must_be_available_if_branch_item_available
