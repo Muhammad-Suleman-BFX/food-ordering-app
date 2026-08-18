8793892e8d2cdf39095ffbad5ca0a7b6e20b0dd2
muhammad-suleman-bfx <muhammadsuleman.tahir@albaik.com>
2026-08-18 16:05:31 +0300

    chore: ran migration for order column name changes from type to order_type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_17_000007) do
+ActiveRecord::Schema[7.2].define(version: 2026_08_18_125421) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,7 +74,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_17_000007) do
 
   create_table "orders", force: :cascade do |t|
     t.bigint "branch_id", null: false
-    t.integer "type", default: 0, null: false
+    t.integer "order_type", default: 0, null: false
     t.integer "status", default: 0, null: false
     t.decimal "total_price", precision: 10, scale: 2, default: "0.0", null: false
     t.datetime "created_at", null: false
