9a149b9e1229149687818be7fc84c3e3bbce8c0b
muhammad-suleman-bfx <muhammadsuleman.tahir@albaik.com>
2026-08-18 16:54:27 +0300

    fix: column name updated in cart_item from branch_menu_item_quantity to quantity

--- db/migrate/20250817000005_create_cart_items.rb
+++ db/migrate/20250817000005_create_cart_items.rb
@@ -4,7 +4,7 @@ class CreateCartItems < ActiveRecord::Migration[7.1]
       t.references :cart, null: false, foreign_key: true
       t.references :branch_menu_item, null: false, foreign_key: true
 
-      t.integer :branch_menu_item_quantity, null: false, default: 1
+      t.integer :quantity, null: false, default: 1
 
       t.timestamps
     end
