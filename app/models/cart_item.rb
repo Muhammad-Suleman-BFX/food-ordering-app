69c9ea995858a48ec5b347f7be50792bc15e56d0
muhammad-suleman-bfx <muhammadsuleman.tahir@albaik.com>
2026-08-18 16:54:52 +0300

    chore: added validation in cart_item model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -6,4 +6,17 @@ class CartItem < ApplicationRecord
   validates :branch_menu_item_id, uniqueness: { scope: :cart_id, message: "is already in the cart" }
   validate :branch_menu_item_must_be_available
   validate :must_belong_to_same_branch_as_cart
+
+  private
+  def branch_menu_item_must_be_available
+    if branch_menu_item.present? && !branch_menu_item.effective_available?
+      errors.add(:branch_menu_item, "is not available for order")
+    end
+  end
+
+  def must_belong_to_same_branch_as_cart
+    if cart.present? && branch_menu_item.present? && branch_menu_item.branch_id != cart.branch_id
+      errors.add(:branch_menu_item, "must belong to the same branch as the cart")
+    end
+  end
 end
