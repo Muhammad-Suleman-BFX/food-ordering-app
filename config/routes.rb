2529eb20923bd56083cd267a637cfa590a145c20
muhammad-suleman-bfx <muhammadsuleman.tahir@albaik.com>
2026-08-20 01:51:16 +0300

    feat: updated integration of crud flow for branch menu item

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "branches#index"
-  resources :branches
+  resources :branches do
+    get :menu
+    resources :branch_menu_items,
+      only: [ :new, :create, :edit, :update, :destroy ]
+  end
   resources :menu_items
+  resources :carts
 end
