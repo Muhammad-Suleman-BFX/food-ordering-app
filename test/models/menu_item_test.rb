04eea409572bf4ae6c031f671b6785ee2d844665
muhammad-suleman-bfx <muhammadsuleman.tahir@albaik.com>
2026-08-24 18:43:13 +0300

    added new test cases

---
+++ test/models/menu_item_test.rb
@@ -0,0 +1,70 @@
+require "test_helper"
+
+class MenuItemTest < ActiveSupport::TestCase
+  def setup
+    @menu_item = MenuItem.new(
+      name: "Broasted Chicken — 4 Pcs",
+      description: "Crispy golden broasted chicken",
+      base_availability: true
+    )
+  end
+
+  test "should be valid with all attributes" do
+    assert @menu_item.valid?
+  end
+
+  test "should be invalid without name" do
+    @menu_item.name = nil
+    assert_not @menu_item.valid?
+    assert_includes @menu_item.errors[:name], "can't be blank"
+  end
+
+  test "should be invalid with name longer than 255 characters" do
+    @menu_item.name = "a" * 256
+    assert_not @menu_item.valid?
+    assert_includes @menu_item.errors[:name], "is too long (maximum is 255 characters)"
+  end
+
+  test "should be invalid with description longer than 2000 characters" do
+    @menu_item.description = "a" * 2001
+    assert_not @menu_item.valid?
+    assert_includes @menu_item.errors[:description], "is too long (maximum is 2000 characters)"
+  end
+
+  test "should be valid without description" do
+    @menu_item.description = nil
+    assert @menu_item.valid?
+  end
+
+  test "should be invalid without base_availability" do
+    @menu_item.base_availability = nil
+    assert_not @menu_item.valid?
+    assert_includes @menu_item.errors[:base_availability], "is not included in the list"
+  end
+
+  test "should be valid when base_availability is false" do
+    @menu_item.base_availability = false
+    assert @menu_item.valid?
+  end
+
+  test "should destroy associated branch_menu_items when destroyed" do
+    menu_item = menu_items(:chicken_4pc)
+    assert_difference "BranchMenuItem.count", -1 do
+      menu_item.destroy
+    end
+  end
+
+  test "should have many branch_menu_items" do
+    menu_item = menu_items(:chicken_8pc)
+    assert_equal 2, menu_item.branch_menu_items.count
+    assert_includes menu_item.branch_menu_items.map(&:branch), branches(:jeddah)
+    assert_includes menu_item.branch_menu_items.map(&:branch), branches(:makkah)
+  end
+
+  test "should have many branches through branch_menu_items" do
+    menu_item = menu_items(:chicken_8pc)
+    assert_equal 2, menu_item.branches.count
+    assert_includes menu_item.branches, branches(:jeddah)
+    assert_includes menu_item.branches, branches(:makkah)
+  end
+end
