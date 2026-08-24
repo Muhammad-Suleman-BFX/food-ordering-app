c6e15c8cdbd21a392912bb75cd01bb7bec202058
muhammad-suleman-bfx <muhammadsuleman.tahir@albaik.com>
2026-08-24 18:44:20 +0300

    handled edge cases

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  private
+
+  def render_404
+    render file: Rails.root.join("public/404.html"), status: :not_found, layout: false
+  end
 end
